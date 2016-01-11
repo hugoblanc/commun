79a20b2278f20f7d619acb13d59c49595a423857
BobBDE <benji69___@hotmail.fr>
2016-01-11 17:22:39 +0100

    Modification2

--- controleurs/loginCtrl.js
+++ controleurs/loginCtrl.js
@@ -1,7 +1,4 @@
-  /*var userStocked =  JSON.parse(window.localStorage.getItem("currentUser"));
-  user.mdp = userStocked.mdp;
-  user.pseudo = userStocked.pseudo;*/
-app.controller('LoginCtrl', function (Backand, $scope, $state, Connexion, $http, $rootScope) {
+app.controller('LoginCtrl', function (Backand, $scope, $state, ServiceLogin) {
     $scope.lblPseudo = false;
     $scope.lblMdp = false;
     $scope.error = "";
@@ -18,82 +15,83 @@ app.controller('LoginCtrl', function (Backand, $scope, $state, Connexion, $http,
                 $scope.lblMdp = true;
 
         } else {
-            //$scope.result = '  '+ user.mdp + '    ' + user.pseudo;
-            Connexion.signin(user.pseudo, user.mdp)
+            //connexion 
+            ServiceLogin.signin(user.pseudo, user.mdp)
                     .then(
                             function (result) {
-                                //$rootScope.user.id = $scope.getUserId(user.pseudo);
-                                $scope.getUserId(user.pseudo);
-                                $scope.erreur = "";
-//                                window.localStorage.setItem("user", JSON.stringify(user));
-//                                window.localStorage.setItem("token", JSON.stringify(result));
-                                $state.go('accueil');
+                                //stoker les infos du user en local
+                                currentUser = {};
+                                currentUser.fullName = result.fullName;
+                                currentUser.role = result.role;
+                                currentUser.username = result.username;
+                                //currentUser.token = result.access_token;
+
+                                //recupérer les infos du user
+                                ServiceLogin.getUserInfos(result.username).then(function (result) {
+                                    //si on a récupérer l'id
+                                    $scope.erreur = "";
+                                    //stocker l'id
+                                    currentUser.id =  result.data[0].id;
+                                    currentUser.filiere =  result.data[0].filiere;
+                                    currentUser.annee =  result.data[0].annee;
+                                    //mettre l'objet currentUser en local
+                                    window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+//                                    $rootScope.currentUser = currentUser;
+                                    //aller a la page tab.acceuil
+                                    $state.go('tab.accueil');                        
+////                                 window.localStorage.setItem("token", JSON.stringify(result));                                  
+                                }, function (data) {
+                                    $scope.erreur = "Erreur de connexion à la base";
+                                });
                             },
                             function (data) {
-                                if(data.error === "invalid_grant"){
+                                if (data.error === "invalid_grant") {
                                     $scope.error = "Email ou mot de passe incorrect";
+                                } else {
+                                    $scope.error = "Erreur inconnu, veuillez réessayer plus tard";
                                 }
-                                else{
-                                    $scope.error = "Erreur inconnu, veuillez rï¿½essayer plus tard"; 
-                                }
-                               
+
                                 console.log(data);
                             }
                     );
         }
     }
 
-    $scope.getUserId = function (email) {
-        $http({
-            method: 'GET',
-            url: Backand.getApiUrl() + '/1/query/data/GetUser',
-            params: {
-                parameters: {
-                    email: email
-                }
-            }
-        }).then(function (result) {
-            window.localStorage.setItem("userId", JSON.stringify(result.data[0].id));
-        }, function (data) {
-            console.log(data);
-        });
-    };
-    
 
     //Sign up to Backand
     function signup(form) {
-        if( !checkEmail(form.email) ){
+        if (!checkEmail(form.email)) {
             $scope.error = "Email invalide, vous devez rentrer un email de CPE.";
             return;
         }
         return Backand.signup(form.firstName, form.lastName, form.email,
                 form.password, form.password, {filiere: form.filiere, annee: form.annee}
-                )
+        )
                 .then(function (response) {
                     //$rootScope.user.id = $scope.getUserId(user.pseudo);
                     $scope.error = "";
                     $state.go('verifEmail');
-                    
-                }, 
-                function (data) {
-                    $scope.error = data.data;
-                    console.log(data);
-                });
-    };
-    
-    function checkEmail(email){
-       return true;
-      //return email.substr(email.length - 7) === "@cpe.fr";
-    };
-    
-    $scope.goToResetPassword = function(){
-       $state.go("resetPassword");
-    };
-    
-    
 
-      window.localStorage.setItem("user", JSON.stringify(user));
-      window.localStorage.setItem("token", JSON.stringify(result));
+                },
+                        function (data) {
+                            $scope.error = data.data;
+                            console.log(data);
+                        });
+    }
+    ;
+
+    function checkEmail(email) {
+        return true;
+        //return email.substr(email.length - 7) === "@cpe.fr";
+    }
+    ;
+
+    $scope.goToResetPassword = function () {
+        $state.go("resetPassword");
+    };
+
+
+
 
     $scope.login = login;
     $scope.signup = signup;
